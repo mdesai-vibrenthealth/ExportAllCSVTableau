9235039578710979e5b6c8410bc4e2a675faa2d9
mdesai-vibrenthealth <61553431+mdesai-vibrenthealth@users.noreply.github.com>
2020-06-25 18:35:22 +0530

    pushing file

---
+++ precache-manifest.94b6712092a3f1334bb438ffe7789bb0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfc3001a4528f1033e66d4ae43ae51a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b18ff94f5c378a15f2d",
+    "url": "/static/css/main.7bdf0d04.chunk.css"
+  },
+  {
+    "revision": "77a56312b56f0780fabe",
+    "url": "/static/js/2.2435c0e7.chunk.js"
+  },
+  {
+    "revision": "fcf4f6d3b886ef447aa7d2c266f2141a",
+    "url": "/static/js/2.2435c0e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b18ff94f5c378a15f2d",
+    "url": "/static/js/main.800f131a.chunk.js"
+  },
+  {
+    "revision": "25f4ef40237fb0a9b6e8",
+    "url": "/static/js/runtime-main.0b269949.js"
+  },
+  {
+    "revision": "78313ace4e54623190f1ee1b59a24058",
+    "url": "/static/media/logo.78313ace.svg"
+  }
+]);
